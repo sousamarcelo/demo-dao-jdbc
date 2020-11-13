af210b5fc9138e1f4a20a2dc9c1ef9be56730ec0
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2020-11-13 12:22:13 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 		
 		Department dp = new Department(1, "Produtos");
 		Seller sl = new Seller(1, "Marcelo", "marcelo@gmail", new Date(), 3000.00, dp);
+		
+		SellerDao seller = DaoFactory.createSellerDao();
 		
 		System.out.println(dp);
 		System.out.println(sl);
