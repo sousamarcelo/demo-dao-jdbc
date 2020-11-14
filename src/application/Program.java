f909074ac767e7a406781b67aba6bddb9b56d089
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2020-11-14 11:01:53 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+	
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Department dp = new Department(1, "Produtos");
-		Seller sl = new Seller(1, "Marcelo", "marcelo@gmail", new Date(), 3000.00, dp);
+		Seller seller = sellerDao.findById(3);
 		
-		SellerDao seller = DaoFactory.createSellerDao();
-		
-		System.out.println(dp);
-		System.out.println(sl);
+		System.out.println(seller);
 
 	}
 
