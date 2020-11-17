8044c5a031901367d0aba99d54c74c144aa08fcf
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2020-11-17 00:50:41 -0300

    Continuando exercicio CRUD para o do Department, Inserção, alteração e deleção ja foram incluidos com sucesso

---
+++ src/application/Program2.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.util.Scanner;
+
+import db.DB;
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		Department newDepartment = null;
+
+		System.out.println("===== TEST 1: Department insert =====");
+		System.out.print("Name new department: ");
+		String nameDepartment = sc.nextLine();
+		newDepartment = new Department(null, nameDepartment);
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! new Department: " + newDepartment.getId());
+		System.out.println("Name: " + newDepartment.getName());
+
+		System.out.println("\n===== TEST 2: Department updated =====");
+		System.out.print("Id department updated: ");
+		Integer id = sc.nextInt();
+		System.out.print("Name department updated: ");
+		sc.nextLine();
+		String nameUpdated = sc.nextLine();
+		newDepartment = new Department(id, nameUpdated);
+		departmentDao.update(newDepartment);
+
+		System.out.println("\n===== TEST 3: Department deleted =====");
+		System.out.print("Id departmento deleted:? ");
+		Integer idDeleted = sc.nextInt();
+		departmentDao.deleteById(idDeleted);
+		System.out.println("Id " + idDeleted + " deleted sucess");
+
+		System.out.println("\n===== TEST 4: Department find By Id =====");
+
+		sc.close();
+		System.out.println();
+		DB.closeConnection();
+
+	}
+
+}
